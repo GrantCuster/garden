c4e4d4eaf7476b3504c2edd8640f5e8d21400512
Grant Custer <grantcuster@gmail.com>
2024-06-15 07:20:34 -0400

    transitioning to garden

---
+++ index.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+  // Open external links in a new tab
+  const postLinks = document.querySelectorAll('.post a');
+  postLinks.forEach((postLink) => {
+    const href = postLink.getAttribute('href');
+    if (href && /^(https?:)?\/\//.test(href)) {
+      postLink.target = '_blank';
+    }
+  });
+
+  // Add click event listener to images
+  const postImages = document.querySelectorAll('.post img');
+  postImages.forEach((postImage) => {
+    postImage.addEventListener('click', () => {
+      const imageContainer = document.createElement('div');
+      imageContainer.classList.add('image-container');
+
+      const image = document.createElement('img');
+      image.src = postImage.src;
+      imageContainer.appendChild(image);
+
+      document.body.appendChild(imageContainer);
+
+      // Add click event listener to remove image container
+      imageContainer.addEventListener('click', () => {
+        document.body.removeChild(imageContainer);
+      });
+    });
+  });
+};
+
